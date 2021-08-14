8361ce86183478a9bf536da563dc5d2c783fcf60
fred <super88a@gmail.com>
2021-08-14 10:57:44 +0700

    v1.0

--- src/main/java/com/primetech/wf/ObtRepository.java
+++ src/main/java/com/primetech/wf/ObtRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ObtRepository extends JpaRepository<Obt, Long> {
 
-	public Obt findByNama(String nama);
+	//public Obt findByNama(String nama);
 
 }
