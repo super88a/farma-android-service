a460d8398e68d86e40331f47aa54b24fe2ec6590
fred <super88a@gmail.com>
2021-08-14 10:35:41 +0700

    v1.0

--- src/main/java/com/primetech/wf/ObtRepository.java
+++ src/main/java/com/primetech/wf/ObtRepository.java
@@ -2,7 +2,7 @@ package com.primetech.wf;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+//import java.util.List;
 
 @Repository
 public interface ObtRepository extends JpaRepository<Obt, Long> {
